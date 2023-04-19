f7eeaf73cd123fb9b31ea5e99d61d55bea3c7a44
chris-kauffmann <chris_kauffmann@hotmail.com>
2023-04-19 16:27:38 -0300

    CRUD farmacia

---
+++ src/test/java/com/example/farmacia/FarmaciagenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciagenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
